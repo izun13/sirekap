3017ec7aa8beb26f223e5da30bf908138e21cc77
izun13 <izun13@gmail.com>
2024-04-18 11:15:02 +0700

    aplikasi rekapitulasi perizinan

--- includes/koneksi.php
+++ includes/koneksi.php
@@ -1,6 +1,6 @@
 <?php
 mysql_connect("localhost","root","");
-//mysql_connect("172.17.20.4","dpmptsp","010101");
+//mysql_connect("","","");
 $con=mysql_select_db("db_perizinan");
 //if($con){echo "OK";}else{echo "KO";}
 ?>
--- includes/konfigurasi_db.php
+++ includes/konfigurasi_db.php
@@ -1,11 +1,11 @@
 <?php
-//mysql_connect("175.106.19.122","dpmptsp","010101");
+//mysql_connect("","","");
 define('DB1_NAMA', 'db_perizinan'); // sesuaikan dengan nama database pertama anda
 define('DB1_USER', 'root'); // sesuaikan dengan nama pengguna database pertama anda
 define('DB1_PASSWORD', ''); // sesuaikan dengan kata sandi database pertama anda
 define('DB1_HOST', 'localhost'); // ganti jika letak database mysql di komputer lain
 
-//mysql_connect("172.17.20.4","admin_bo","uKD97mtPr6");
+//mysql_connect("","","");
 //$con=mysql_select_db("admin_bo");
 define('DB2_NAMA', 'admin_bo'); // sesuaikan dengan nama database kedua anda
 define('DB2_USER', 'root'); // sesuaikan dengan nama pengguna database kedua anda //admin_bo
